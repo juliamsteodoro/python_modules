f4dc57830a23557d950ece5465f064b6cad55c9e
Julia Moreira Dos Santos Teodoro <jumoreir@c3r8p1.42sp.org.br>
2026-09-12 12:04:20 -0300

    unfinished ex02

---
+++ python_01/ex0/ft_garden_intro.py
@@ -0,0 +1,10 @@
+def ft_garden_intro():
+	plant = "Rose"
+	height = 25
+	age = 30
+	print(f"Plant: {plant}\nHeight: {height}cm\nAge: {age} days")
+
+if __name__ == "__main__":
+	print("=== Welcome to My Garden ===")
+	ft_garden_intro()
+	print("\n=== End of Program ===")
---
+++ python_01/ex02/ft_plant_growth.py
@@ -0,0 +1,5 @@
+from ft_garden_data import Plant 
+
+if __name__ == "__main__":
+	print("=== Garden Plant Growth ===")
+	rose = Plant("Rose", 25, 30)
