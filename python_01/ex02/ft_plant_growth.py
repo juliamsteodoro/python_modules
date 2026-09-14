6f2c825b4cf6092f4529467b4ff983a45a6c4429
Julia Moreira Dos Santos Teodoro <jumoreir@c1r1p4.42sp.org.br>
2026-09-14 11:19:23 -0300

    1/2 done

--- python_01/ex02/ft_plant_growth.py
+++ python_01/ex02/ft_plant_growth.py
@@ -1,5 +1,37 @@
-from ft_garden_data import Plant 
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_plant_growth.py                                 :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jumoreir <jumoreir@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/09/14 10:58:04 by jumoreir          #+#    #+#              #
+#    Updated: 2026/09/14 10:58:06 by jumoreir         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+class Plant:
+	def __init__(self, name, height, age):
+		self.name = name
+		self.height = height
+		self.age = age
+		
+	def grow(self):
+		self.height += 0.8
+
+	def age_up(self):
+		self.age += 1
+	def show(self):
+		print(f"{self.name}: {round(self.height, 1)}cm, {self.age} days old")
 
 if __name__ == "__main__":
 	print("=== Garden Plant Growth ===")
 	rose = Plant("Rose", 25, 30)
+	initial_height = rose.height
+	rose.show()
+	for i in range(1, 8):
+		print(f"=== Day {i} ===")
+		rose.grow()
+		rose.age_up()
+		rose.show()
+	print(f"Growth this week: {round((rose.height - initial_height), 1)}cm")
--- python_01/ex03/ft_plant_factory.py
+++ python_01/ex03/ft_plant_factory.py
@@ -1,22 +1,31 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    ft_garden_intro.py                                 :+:      :+:    :+:    #
+#    ft_plant_factory.py                                :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: jumoreir <jumoreir@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/09/14 10:58:18 by jumoreir          #+#    #+#              #
-#    Updated: 2026/09/14 10:58:20 by jumoreir         ###   ########.fr        #
+#    Created: 2026/09/14 10:57:56 by jumoreir          #+#    #+#              #
+#    Updated: 2026/09/14 11:18:05 by jumoreir         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-def ft_garden_intro():
-	plant = "Rose"
-	height = 25
-	age = 30
-	print(f"Plant: {plant}\nHeight: {height}cm\nAge: {age} days")
+class Plant:
+	def __init__(self, name, height, age):
+		self.name = name
+		self.height = height
+		self.age = age
+	def show(self):
+		print(f"Created: {self.name}: {round(self.height, 1)}cm, {self.age} days old")
 
 if __name__ == "__main__":
-	print("=== Welcome to My Garden ===")
-	ft_garden_intro()
-	print("\n=== End of Program ===")
+	print("=== Plant Factory Output ===")
+	plants = [
+		Plant("Rose", 25, 30),
+		Plant("Oak", 200, 365),
+		Plant("Cactus", 5, 90),
+		Plant("Sunflower", 80, 45),
+		Plant("Fern", 15, 120),
+	]
+	for plant in plants:
+		plant.show()
